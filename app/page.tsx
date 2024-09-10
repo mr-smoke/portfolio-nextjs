c3ad74abee7d1b5c99c661c4273564c3f9a579ac
baki duman <g.s_baki_70@hotmail.com>
2024-09-10 21:12:21 +0300

    Comments section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Comments from "@/components/Comments";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -9,6 +10,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Comments />
       </div>
     </main>
   );
---
+++ components/Comments.tsx
@@ -0,0 +1,18 @@
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { testimonials } from "@/data";
+
+const Comments = () => {
+  return (
+    <section className="py-20">
+      <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold text-center dark:text-blue-100 tracking-wide">
+        Comments from
+        <span className="text-fuchsia-500"> some clients</span>
+      </h2>
+      <div className="mt-10 antialiased">
+        <InfiniteMovingCards items={testimonials} speed="slow" />
+      </div>
+    </section>
+  );
+};
+
+export default Comments;
